3cdb24a3882ee2b16bd4726fd1038749918a067a
yanshihang <2495872823@qq.com>
2023-05-20 21:05:36 +0800

    增加home页面

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -3,15 +3,16 @@ import Vue from 'vue';
 import LoginComponent from './components/LoginComponent.vue';
 import AdminLogin from './components/AdminLogin.vue'
 import RegisterComponent from './components/RegisterComponent.vue';
+import HomeComponent from './components/HomeComponent.vue'
 import App from './App.vue'
 Vue.use(VueRouter);
 
 const routes = [
-    { path: '/login', component: LoginComponent, name: 'login' },
-    { path: '/adminlogin', component: AdminLogin, name: 'register' },
-    { path: '/register', component: RegisterComponent, name: 'register' },
-    { path: '/', component: App, name: '/' },
-
+    { path: '/login', component: LoginComponent, name: 'login',meta:{showHome:false}},
+    { path: '/adminlogin', component: AdminLogin, name: 'register',meta:{showHome:false}},
+    { path: '/register', component: RegisterComponent, name: 'register',meta:{showHome:false}},
+    { path: '/', component: App, name: '/',meta:{showHome:true}},
+    { path: '/home', component: HomeComponent, name: 'home',meta:{showHome:true}},
     // 其他路由配置
 ];
 
